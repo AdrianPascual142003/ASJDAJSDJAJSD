45f8c30ae4e771525da72ebf3d6787f825695bb7
Rubén Catalán Juan <rucaju5@gmail.com>
2022-05-17 18:35:36 +0200

    First commit

---
+++ src/test/java/es/g101/crosstube/CrosstubeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.g101.crosstube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrosstubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
